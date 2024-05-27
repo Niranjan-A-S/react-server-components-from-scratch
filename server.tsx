ff413226d4e4bd8b72a41e7aa6970d77a1d66d01
Niranjan A S <niranjan0881@gmail.com>
2024-05-27 12:39:54 +0000

    chore: implemented support for server components

--- server.tsx
+++ server.tsx
@@ -4,6 +4,37 @@ import path from "path";
 import { Layout } from "./layout";
 import React from "react";
 
+const turnJSXIntoClientObject: any = async (jsx: any) => {
+    if (!jsx) return null;
+
+    if (['string', 'number', 'boolean'].includes(typeof jsx)) return jsx;
+
+    if (Array.isArray(jsx)) return await Promise.all(jsx.map(turnJSXIntoClientObject));
+
+    if (typeof jsx === 'object') {
+        if (jsx.$$typeof === Symbol.for('react.element')) {
+            if (typeof jsx.type === 'string') {
+                return {
+                    ...jsx,
+                    props: await turnJSXIntoClientObject(jsx.props)
+                }
+            }
+            if (typeof jsx.type === 'function') {
+                const Component = jsx.type;
+                const props = jsx.props;
+                const renderedThing = await Component(props);
+                return await turnJSXIntoClientObject(renderedThing);
+            }
+        }
+
+        const entries = Object.entries(jsx);
+        const processedEntries: any = await Promise.all(entries.map(async ([props, value]) => [props, await turnJSXIntoClientObject(value)]));
+        return Object.fromEntries(processedEntries);
+    }
+
+}
+
+
 const app = express();
 
 app.use(express.static("dist"));
@@ -13,13 +44,15 @@ const PORT = 3000;
 app.get("/:path", async (req, res) => {
     const page = await import(path.join(process.cwd(), 'dist', 'pages', req.params.path));
     const Component = page.default;
-    const html = renderToString(
+    const clientJSX = await turnJSXIntoClientObject(
         <Layout bgColor="white">
             <Component {...req.query}/>
         </Layout>
     );
-    res.end(`${html}`);
-})
+    const html = renderToString(clientJSX);
+    res.end(html);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Application running on port ${PORT}`)
