19228d5666e917250d18111185e8bd9be36e69b6
Niranjan A S <niranjan0881@gmail.com>
2024-05-27 12:32:42 +0000

    chore: changed to default import

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { Layout } from "./layout";
 import  List  from "./pages/list";
-import  Detail  from "./pages/details";
+import Detail from "./pages/detail";
 
 const searchParams = new URLSearchParams(location.search);
 const breed = searchParams.get("breed");
